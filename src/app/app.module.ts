9367ba33c14ac0fa10c48db82ae03362767d4e70
BrendaComas <brendacomasok@gmail.com>
2023-04-28 16:46:29 -0300

    se agrega el proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import { ServicioComponent } from './servicio/servicio.component';
+
+@NgModule({
+  declarations: [			
+    AppComponent,
+    HeaderComponent,
+
+  ServicioComponent,
+     
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
